6038c16d5846f7c084d109d2bba30d8204648cee
DeveloperJosh <39308600+DeveloperJosh@users.noreply.github.com>
2020-06-02 07:12:43 -0400

    Update email.py

--- email.py
+++ email.py
@@ -5,8 +5,8 @@ from string import Template
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-MY_ADDRESS = 'bam0909@outlook.com'
-PASSWORD = 'gunner09'
+MY_ADDRESS = ''
+PASSWORD = ''
 
 def get_contacts(filename):
     
