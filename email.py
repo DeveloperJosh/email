29d85ae156afbd7d5c38a23b5114d2073854210e
DeveloperJosh <39308600+DeveloperJosh@users.noreply.github.com>
2020-06-02 07:11:41 -0400

    Add files via upload

---
+++ email.py
@@ -0,0 +1,62 @@
+import smtplib
+
+from string import Template
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+MY_ADDRESS = 'bam0909@outlook.com'
+PASSWORD = 'gunner09'
+
+def get_contacts(filename):
+    
+    names = []
+    emails = []
+    with open(filename, mode='r', encoding='utf-8') as contacts_file:
+        for a_contact in contacts_file:
+            names.append(a_contact.split()[0])
+            emails.append(a_contact.split()[1])
+    return names, emails
+
+def read_template(filename):
+    
+    with open(filename, 'r', encoding='utf-8') as template_file:
+        template_file_content = template_file.read()
+    return Template(template_file_content)
+
+def main():
+    names, emails = get_contacts('mycontacts.txt') # read contacts
+    message_template = read_template('message.txt')
+
+    # set up the SMTP server
+    s = smtplib.SMTP(host='smtp.sendgrid.net', port=465)
+    s.starttls()
+    s.login(apikey, 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)
+
+    # For each contact, send the email:
+    for name, email in zip(names, emails):
+        msg = MIMEMultipart()       # create a message
+
+        # add in the actual person name to the message template
+        message = message_template.substitute(PERSON_NAME=name.title())
+
+        # Prints out the message body for our sake
+        print(message)
+
+        # setup the parameters of the message
+        msg['From']=Godly-Gamers
+        msg['To']=Joshwells409@gmail.com
+        msg['Subject']="Hey"
+        
+        # add in the message body
+        msg.attach(MIMEText(message, 'plain'))
+        
+        # send the message via the server set up earlier.
+        s.send_message(msg)
+        del msg
+        
+    # Terminate the SMTP session and close the connection
+    s.quit()
+    
+if __name__ == '__main__':
+    main()
